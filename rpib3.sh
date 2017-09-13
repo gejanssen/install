d897fed2abd2a3c55316dbf9750bbc79bf454b45
gejanssen <github@gejanssen.com>
2017-09-13 14:45:41 +0200

    zero serial

--- rpib3.sh
+++ rpib3.sh
@@ -1,2 +1,5 @@
 # serial settings
-echo "core_freq=250" >> /boot/config.txt
+#echo "core_freq=250" >> /boot/config.txt
+echo >> /boot/config.txt
+echo "#Enable UART" >> /boot/config.txt
+echo "enable_uart=1" >> /boot/config.txt
